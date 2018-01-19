287e33e20d5ef98a21853ee0ad5fb98e0bc9f8e9
Diego <diego.gliosca@gmail.com>
2018-01-19 16:23:13 +0000

    added acceptance test

---
+++ src/test/kotlin/DVDCoverCollectorTest.kt
@@ -0,0 +1,45 @@
+import junit.framework.Assert.fail
+import org.http4k.client.ApacheClient
+import org.http4k.core.Method.GET
+import org.http4k.core.Request
+import org.junit.Ignore
+import org.junit.Test
+import java.io.*
+
+class DVDCoverCollectorTest {
+
+    @Ignore
+    @Test fun `can get a cover`() {
+        val movie = "avatar"
+        val request = Request(GET, "http://dvdcover.com/search/$movie")
+
+        val client = ApacheClient()
+
+        val response = client(request)
+
+        val actualImage = response.body.stream
+        val expectedImage = javaClass.classLoader.getResourceAsStream("avatar.jpg")
+
+        assertBinaryContentIsTheSame(actualImage, expectedImage)
+    }
+
+    private fun assertBinaryContentIsTheSame(binaryInputA: InputStream, binaryInputB: InputStream): Boolean {
+        val byteReadA = binaryInputA.buffered()
+        val byteReadB = binaryInputB.buffered()
+
+        byteReadA.use { readA ->
+            byteReadB.use { readB ->
+                val byteArrayA = ByteArray(1)
+                val byteArrayB = ByteArray(1)
+                do {
+                    val contentA = readA.read(byteArrayA)
+                    val contentB = readB.read(byteArrayB)
+                    val match = (contentA == contentB) && byteArrayA.contentEquals(byteArrayB)
+                    if (!match)
+                        fail("The content is different")
+                } while (contentA != -1 && contentB != -1)
+            }
+        }
+        return true
+    }
+}
